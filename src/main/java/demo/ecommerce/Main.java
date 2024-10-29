23d6e9027216f59f49fe07634ea6ab1eb8e3d7c9
Bekzod <bekzodnosirov02@gmail.com>
2024-10-29 11:26:48 +0500

    addToArrayForm

--- src/main/java/demo/ecommerce/Main.java
+++ src/main/java/demo/ecommerce/Main.java
@@ -1,10 +1,29 @@
 package demo.ecommerce;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        int[] a = {9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
+        int k = 1;
+        System.out.println(addToArrayForm(a, k));
     }
 
-
+    public static List<Integer> addToArrayForm(int[] num, int k) {
+        StringBuilder s = new StringBuilder();
+        for (Integer i : num) {
+            s.append(i);
+        }
+        int n = 0;
+        if (!s.isEmpty()) n = Integer.parseInt(s.toString());
+        int a = n + k;
+        List<Integer> res = new ArrayList<Integer>();
+        for (String d : Integer.toString(a).split("")) {
+            res.add(Integer.parseInt(d));
+        }
+        return res;
+    }
 
 }
